02e557222a1a4c8b1efa90a46862fec92cd5ab71
Zuzeng Lin <linzuzeng@hotmail.com>
2018-03-10 19:30:02 +0100

    Signed-off-by: Zuzeng Lin <linzuzeng@hotmail.com>

--- eta.py
+++ eta.py
@@ -1,60 +1,17 @@
-import numba as nb
-from numba import jit
-from llvmlite import ir, binding as ll
 import numpy as np
-import sys
-
-@nb.extending.intrinsic
-def PARSE_TimeTagFileHeader_link(typingctx):
-    sig = nb.typing.signature(nb.int32)
-    func_name = "PARSE_TimeTagFileHeader"
-    def compile_library(context, asm):
-        library = context.codegen().create_library('compiled_module')
-        ll_module = ll.parse_assembly(asm)
-        ll_module.verify()
-        library.add_llvm_module(ll_module)
-        return library
-
-    def codegen(context, builder, sig, args):
-        # print("============")
-        with open(func_name + ".ll", "r") as f:
-            assembly = f.read()
-        library = compile_library(context, assembly)
-        codegen.libs = [library]   # Weird hack to get the library linked
-        return context.get_constant(nb.int32, 42)
-
-    return sig, codegen
-@nb.extending.intrinsic
-def PARSE_TimeTagFileHeader(typingctx, pram1):
-
-    sig = nb.typing.signature(nb.int32, pram1)
-    func_name = "PARSE_TimeTagFileHeader"
-    def codegen(context, builder, sig, args):
-        argtypes = [context.get_argument_type(aty) for aty in sig.args]
-        restype = context.get_argument_type(sig.return_type)
-        fnty = ir.FunctionType(restype, argtypes)
-        fn = nb.cgutils.insert_pure_function(
-            builder.module, fnty, name=func_name)
-        retval = context.call_external_function(builder, fn, sig.args, args)
-        print(fn)
-        return retval
-
-    return sig, codegen
-
-import cffi
-ffi = cffi.FFI()
-
-
-@jit(nopython=True, parallel=True,nogil=True)
-def foo(a,b):
+from jit_linker import *
+@jit(nopython=True, parallel=True, nogil=True)
+def foo(a, b):
+    link_libs()
+    #read file header
     aa = ffi.from_buffer(a)
     bb = ffi.from_buffer(b)
-    PARSE_TimeTagFileHeader_link()
-    ret1=PARSE_TimeTagFileHeader(aa)
-    ret2= PARSE_TimeTagFileHeader(bb)
-    return ret1+ret2
-#operatee = np.ones(10, dtype=np.int64)
-out = foo(bytearray("t2.ptu","ascii"),bytearray("qt2.ptu","ascii"))
+    ret1 = PARSE_TimeTagFileHeader(aa)
+    ret2 = PARSE_TimeTagFileHeader(bb)
+    return ret1 + ret2
+
+# operatee = np.ones(10, dtype=np.int64)
+out = foo(bytearray("t2.ptu", "ascii"), bytearray("qt2.ptu", "ascii"))
 
 
 with open("llvm.txt", "w") as writeto:
---
+++ jit_linker.py
@@ -0,0 +1,55 @@
+from llvmlite import ir, binding as ll
+import numba as nb
+from os import listdir
+from os.path import isfile, join
+from numba import jit
+import cffi
+ffi = cffi.FFI()
+ll_path = ".\\ll\\"
+
+@nb.extending.intrinsic
+def link_libs(typingctx):
+    sig = nb.typing.signature(nb.int32)
+
+    def compile_library(context, asm, libname='compiled_module'):
+        library = context.codegen().create_library(libname)
+        ll_module = ll.parse_assembly(asm)
+        ll_module.verify()
+        library.add_llvm_module(ll_module)
+        return library
+
+    def codegen(context, builder, sig, args):
+        print("===== linking =====")
+        codegen.libs = []   # Weird hack to get the library linked
+        for f in listdir(ll_path):
+            lib_path = join(ll_path, f)
+            if isfile(lib_path):
+
+                print(lib_path)
+                with open(lib_path, "r") as fio:
+                    assembly = fio.read()
+                    library = compile_library(context, assembly, lib_path)
+                codegen.libs.append(library)
+        print("===== done =====")
+        return context.get_constant(nb.int32, 42)
+
+    return sig, codegen
+
+
+@nb.extending.intrinsic
+def PARSE_TimeTagFileHeader(typingctx, pram1):
+
+    sig = nb.typing.signature(nb.int32, pram1)
+    func_name = "PARSE_TimeTagFileHeader"
+
+    def codegen(context, builder, sig, args):
+        argtypes = [context.get_argument_type(aty) for aty in sig.args]
+        restype = context.get_argument_type(sig.return_type)
+        fnty = ir.FunctionType(restype, argtypes)
+        fn = nb.cgutils.insert_pure_function(
+            builder.module, fnty, name=func_name)
+        retval = context.call_external_function(builder, fn, sig.args, args)
+        print(fn)
+        return retval
+
+    return sig, codegen
