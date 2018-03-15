7d50fd43ef283984c8e639fb31b4c812789434e5
Zuzeng Lin <linzuzeng@hotmail.com>
2018-03-15 13:38:16 +0100

    Signed-off-by: Zuzeng Lin <linzuzeng@hotmail.com>

--- eta.py
+++ eta.py
@@ -1,66 +1,30 @@
-import multiprocessing
-import tensor
-from timeit import timeit
-from mainloop import mainloop
-from parser_header import parse_header
-import numpy as np
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
+app = dash.Dash()
 
-def f(caller_parms):
-    print(caller_parms)
-    histogram = np.zeros(62502, dtype=np.int64)
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
 
-    def internal():
-        print("\n", mainloop(np.zeros(1, dtype=np.int8), histogram,
-                             bytearray("HHT2.ptu", "ascii"), *caller_parms))
-    internal()
-    print(histogram)
-    return(histogram)
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
 
-
-@timeit
-def mp_core(caller_parms):
-    print("MP core started")
-    with multiprocessing.Pool(5) as p:
-        ret = p.map(f, caller_parms)
-        print("MP core stopped")
-        return ret
-
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montr¨¦al'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
 
 if __name__ == '__main__':
-    multiprocessing.freeze_support()
-    out = parse_header(bytearray("HHT2.ptu", "ascii"))
-    THREAD_MAX = 2
-    TTF_header_offset = out[0]
-    TTF_filesize = out[1]
-    BytesofRecords = out[2]
-    NumRecords = out[6]
-    caller_parms = []
-    for i in range(THREAD_MAX):
-        # fine-cutter
-        start_point = int(NumRecords / THREAD_MAX) * \
-            BytesofRecords * i + TTF_header_offset
-        stop_point = int(NumRecords / THREAD_MAX) * \
-            BytesofRecords * (i + 1) + TTF_header_offset
-        if (stop_point > TTF_filesize):
-            stop_point = TTF_filesize
-        if (stop_point - start_point > BytesofRecords):
-            caller_parms.append(
-                [start_point, stop_point, out[2], out[3], out[4], out[5]])
-            print(start_point, stop_point)
-
-    ret = mp_core(caller_parms)
-    histogram = np.zeros(62502, dtype=np.int64)
-
-    for each in range(len(ret)):
-        print(caller_parms[each])
-        print(ret[each])
-        histogram += ret[each]
-    with open("etanxg.tensor", "w") as writeto:
-            writeto.write(tensor.print_tensor(histogram.tolist()))
-    with open("llvm.txt", "w") as writeto:
-            codelist = mainloop.inspect_llvm()
-            for each in codelist:
-                writeto.write(str(each))
-                writeto.write("//////////////")
-                writeto.write(codelist[each])
+    app.run_server(debug=True)
