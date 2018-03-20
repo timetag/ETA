135178d19e0ddc39d4212c3fca56f57f1c29d194
Zuzeng Lin <linzuzeng@hotmail.com>
2018-03-20 20:10:48 +0100

    clear up
    Signed-off-by: Zuzeng Lin <linzuzeng@hotmail.com>

--- runtime.py
+++ runtime.py
@@ -1,10 +1,31 @@
 import numpy as np
 import multiprocessing
-from scheduler import scheduler
 import time
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from parser_header import parse_header
+def scheduler(filename,THREAD_MAX=1):
+    out = parse_header(bytearray(filename, "ascii"))
+    TTF_header_offset = out[0]
+    TTF_filesize = out[1]
+    BytesofRecords = out[2]
+    NumRecords = out[6]
+    caller_parms = []
+    for i in range(THREAD_MAX):
+        # fine-cutter
+        start_point = int(NumRecords / THREAD_MAX) * \
+            BytesofRecords * i + TTF_header_offset
+        stop_point = int(NumRecords / THREAD_MAX) * \
+            BytesofRecords * (i + 1) + TTF_header_offset
+        if (stop_point > TTF_filesize):
+            stop_point = TTF_filesize
+        if (stop_point - start_point > BytesofRecords):
+            caller_parms.append(
+                [start_point, stop_point, out[2], out[3], out[4], out[5]])
+            print(start_point, stop_point)
+    return caller_parms
+
 
 
 def ETA_MULTITHREAD(filename, code):
@@ -35,5 +56,10 @@ def ETA(filename, wrapper, mainloop,print):
     result = wrapper(caller_parms[0], mainloop)
     te = time.time()
     print('Time: {} ms'.format( (te - ts) * 1000))
-
+    with open("llvm.txt", "w") as writeto:
+        codelist = mainloop.inspect_llvm()
+        for each in codelist:
+            writeto.write(str(each))
+            writeto.write("//////////////")
+            writeto.write(codelist[each])
     return result
