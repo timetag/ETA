3614e19f367c91b6b3bda15cf874b8b81db6a60b
Zuzeng Lin <linzuzeng@hotmail.com>
2018-09-16 11:12:51 +0800

    cleanup source tree

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="etabackend",
+    version="0.5.6",
+    author="ETA Team",
+    description="Extensible Timetag Analyzer",
+    long_description="ETA is a graphical event-driven programming language for time-tag processing.",
+    url="https://timetag.github.io",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL V2",
+        "Operating System :: OS Independent",
+    ],
+)
