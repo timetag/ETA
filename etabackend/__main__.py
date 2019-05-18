5e10d0f42c1154c9499079a53f26fbec4f9ce3ff
Zuzeng Lin <linzuzeng@hotmail.com>
2019-05-18 15:04:56 +0800

    shell:true

--- etabackend/__main__.py
+++ etabackend/__main__.py
@@ -1,4 +1,5 @@
 from subprocess import call
+import sys
 import os.path
 
-call(["python", os.path.join(os.path.dirname(os.path.realpath(__file__) ),"eta.py")])
+call(["python", os.path.join(os.path.dirname(os.path.realpath(__file__) ),"eta.py")], stdout=sys.stdout, stderr=sys.stderr )
