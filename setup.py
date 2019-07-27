cf51282a32c5c4db4daa152405499a8fd12b1311
Zuzeng Lin <linzuzeng@hotmail.com>
2019-07-27 17:48:40 +0800

    fixing qutag support

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="etabackend",
-    version="0.6.2",
+    version="0.6.3",
     author="ETA Team",
     description="Extensible Timetag Analyzer",
     include_package_data=True,
