9bbb595746e7d1238b3da19b8a09ab98f58e2953
Lucas Schweickert <28017534+Cyberfly100@users.noreply.github.com>
2020-01-30 19:05:47 +0100

    Update __main__.py

--- etabackend/__main__.py
+++ etabackend/__main__.py
@@ -1,4 +1,6 @@
 #!/bin/env python
-import backend
-if __name__ == '__main__':
-    backend.main()
+from subprocess import call
+import os.path
+import sys
+
+call([sys.executable, os.path.join(os.path.dirname(os.path.realpath(__file__) ),"eta.py")])
