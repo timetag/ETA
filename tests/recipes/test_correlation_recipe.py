726187eb66a044b030f51d0d96e700a71fe671b5
Samuel Gyger <samuel@gyger.tech>
2020-02-04 14:48:52 +0100

    tests: Add testfiles for qutag and hydraharp with 320 MHz repetition rate of the sync channel (T2, T3 and filtered sync qutag).
    The ground truth files are generated from the current state of the ETA software, so they could be wrong.
    The T3 mode with 320 MHz is too much for the Hydraharp to measure the g2, later an 80MHz file should be added here.

--- tests/recipes/test_correlation_recipe.py
+++ tests/recipes/test_correlation_recipe.py
@@ -15,12 +15,40 @@ recipe_folder = Path(__file__).parent.parent.parent / "gui" / "src" / "renderer"
 class TestCorrelationRecipe:
     """ Tests if the Correlation Recipe delivered with the software does what it should do.
     """
+    # Store data as reference with:
+    # np.savez_compressed(test_folder / "hydraharp" / "data" / "results" / "200204_hydraharp_T3_g2_320MHz.npz", hist1=hist1, hist2=hist2)
 
     def test_correlation_recipe_Hydraharp_T2(self):
-        pass
+        with open(recipe_folder / "Correlation.eta", 'r') as filehandle:
+            recipe_obj = json.load(filehandle)
+
+        eta_engine = etabackend.eta.ETA()
+        eta_engine.compile_eta(recipe_obj)
+        cut=eta_engine.simple_cut(test_folder / "hydraharp" / "data" / "200204_hydraharp_T2_g2_320MHz.timeres", 4)
+        result=eta_engine.run(cut, group='HHT2')
+
+        hist1=result["h3"]
+        hist2=result["h4"]
+
+        with np.load(test_folder / "hydraharp" / "data" / "results" / "200204_hydraharp_T2_g2_320MHz.npz") as ref_data:
+            assert np.allclose(hist1, ref_data['hist1'])
+            assert np.allclose(hist2, ref_data['hist2']) 
 
     def test_correlation_recipe_Hydraharp_T3(self):
-        pass
+        with open(recipe_folder / "Correlation.eta", 'r') as filehandle:
+            recipe_obj = json.load(filehandle)
+
+        eta_engine = etabackend.eta.ETA()
+        eta_engine.compile_eta(recipe_obj)
+        cut=eta_engine.simple_cut(test_folder / "hydraharp" / "data" / "200204_hydraharp_T3_g2_320MHz.timeres", 4)
+        result=eta_engine.run(cut, group='HHT3')
+
+        hist1=result["h3"]
+        hist2=result["h4"]
+        
+        with np.load(test_folder / "hydraharp" / "data" / "results" / "200204_hydraharp_T3_g2_320MHz.npz") as ref_data:
+            assert np.allclose(hist1, ref_data['hist1'])
+            assert np.allclose(hist2, ref_data['hist2']) 
 
     def test_correlation_recipe_Qutag(self):
         with open(recipe_folder / "Correlation.eta", 'r') as filehandle:
@@ -28,18 +56,15 @@ class TestCorrelationRecipe:
 
         eta_engine = etabackend.eta.ETA()
         eta_engine.compile_eta(recipe_obj)
-        cut=eta_engine.simple_cut(test_folder / "qutag" / "data" / "g2_filtered_sync.bin", 4)
+        cut=eta_engine.simple_cut(test_folder / "qutag" / "data" / "19_qutag_g2_sync_filtered_80MHz.qutag", 4)
         result=eta_engine.run(cut, group='quTAG')
 
         hist1=result["h3"]
         hist2=result["h4"]
 
-        with np.load(test_folder / "qutag" / "data" / "results" / "g2_filtered_sync.npz") as ref_data:
+        with np.load(test_folder / "qutag" / "data" / "results" / "19_qutag_g2_sync_filtered_80MHz.npz") as ref_data:
             assert np.allclose(hist1, ref_data['hist1'])
-            assert np.allclose(hist2, ref_data['hist2'])
-
-        print(result)
-        pass        
+            assert np.allclose(hist2, ref_data['hist2'])  
 
     def test_lifetime_recipe(self):
         pass
