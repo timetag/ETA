ce74e42eb28a8c1478f67a826da1fa28bb11ba39
Samuel Gyger <samuel@gyger.tech>
2020-02-06 20:20:23 +0100

    tests: Check that the delay line works as expected.
    Adds a test for the Delay Line. This test needs a file in
    tests/hydraharp/data that is currently not in the github repo because
    it is too large.  If the file does not exist, the test is skipped.
    A smaller file should be provided in the near future.

---
+++ tests/test_eta_timetag_logic.py
@@ -0,0 +1,31 @@
+import pytest
+import logging
+import os
+
+import json
+from pathlib import Path
+
+import etabackend.eta
+
+test_folder = Path(__file__).parent
+
+class TestETATimetagLogic:
+    """ This collects tests that check if the buildin time-correlation logic is still working as expected.
+    """
+
+    def test_delay_line(self, capsys):
+        if not (test_folder / "hydraharp" / "data" / "19_HH400_T3_80MHz_HBT.timeres").exists():
+            pytest.skip("Test File not available")
+
+        with open(test_folder / "data" / "CheckDelayLineRecipe.eta", 'r') as filehandle:
+                recipe_obj = json.load(filehandle)
+
+        eta_engine = etabackend.eta.ETA()
+        eta_engine.compile_eta(recipe_obj)
+        cut=eta_engine.simple_cut(test_folder / "hydraharp" / "data" / "19_HH400_T3_80MHz_HBT.timeres", 4)
+        result=eta_engine.run(cut, group='compile')
+
+        captured = capsys.readouterr()
+        assert "SYNCRate_pspr" in captured.out
+        assert "chronological error detected" not in captured.out
+        assert "delay line error detected" not in captured.out
