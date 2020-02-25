d2979979dbd58165b25921bb411e9416917b99ef
Samuel Gyger <samuel@gyger.tech>
2020-02-25 18:34:38 +0100

    backend: Moved backend to aiohttp and started messing around making the project asyncable. Pain.

--- etabackend/backend.py
+++ etabackend/backend.py
@@ -1,19 +1,21 @@
 import json
 import logging
 import os
+import queue
 import sys
 import time
 import traceback
+import threading
 import pathlib
 
 import asyncio
 import aiohttp
 from aiohttp import web
 
-import etabackend.webinstall as webinstall
-import etabackend.ws_broadcast as ws_broadcast
-from etabackend.eta import ETA, ETACompilationException
+from bokeh.server.server import Server
 
+import etabackend.webinstall as webinstall
+from etabackend.eta import ETA, ETACompilationException
 
 ETA_VERSION = "v0.7.1"
 ETA_BANNER = \
@@ -35,37 +37,39 @@ class Backend():
         self.logger = logging.getLogger(__name__)
         self.logfrontend = logging.getLogger("etabackend.frontend")
 
-        self.hostlisten = os.environ.get('ETA_LISTEN') or "127.0.0.1"
         self.hostip = os.environ.get('ETA_IP') or "localhost"
-        self.hostport = os.environ.get('ETA_PORT') or "5678"
-        self.hosthttpport = os.environ.get('ETA_HTTPPORT') or "5001"
+        self.hostport = int(os.environ.get('ETA_PORT') or "5678")
+        self.hostdashport = int(os.environ.get('ETA_DASHPORT') or "5679")
         self.displaying = False
+
+        # Fix for tornado webserver
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
         self.app = web.Application(logger=logging.getLogger("etabackend.aiohttp"))
         self.app['websockets'] = []
         self.app.add_routes([web.get('/', self.web_redirect),
-                             web.get('/ws', self.websocket_handler),
                              web.get('/index.html', self.web_index, name='default'),
+                             web.get('/ws', self.websocket_handler),
                              web.static('/', BASE_DIR / 'static/', append_version=True),
                             ])
-                            
-        self.logfrontend.addHandler(WebClientHandler(self.send))
+        self.logfrontend.addHandler(WebClientHandler(self.schedule_send))
 
         self.kernel = ETA()
         self.kernel.add_callback('running', lambda: self.schedule_send('', 'running'))
         self.kernel.add_callback('stopped', lambda: self.schedule_send('', 'stopped'))
-        self.kernel.add_callback('update-recipe', lambda: self.recipe_update())
+        self.kernel.add_callback('update-recipe', lambda: self.schedule_recipe_update())
 
         # Monkeypatch a display function to the kernel to support display in recipe.
         # FIXME This is somehow evil, but works. Should be fixed if the Display function is every fixed, it is also not so beautiful.
         self.kernel.display = self.display
         self.kernel.send = self.schedule_send
+        self.kernel.show = self.show
 
         self.logger.info("ETA URL: http://{}:{}".format(self.hostip, self.hostport))
-
+        self.loop = asyncio.get_event_loop()
         if run_forever:
-            web.run_app(self.app, host=self.hostip, port=self.hostport,)
-   
+            web.run_app(self.app, host=self.hostip, port=self.hostport)
+
     async def web_redirect(self, request):
         location = request.app.router['default'].url_for()
         raise web.HTTPFound(location=location)
@@ -74,7 +78,7 @@ class Backend():
         return web.FileResponse(BASE_DIR / 'templates/index.html')
     
     async def websocket_handler(self, request):
-        ws = web.WebSocketResponse(autoping=True)
+        ws = web.WebSocketResponse(autoping=False)
         await ws.prepare(request)
         request.app['websockets'].append(ws)
 
@@ -84,7 +88,7 @@ class Backend():
             if msg.type == aiohttp.WSMsgType.TEXT:
                 obj = msg.json()
                 self.logger.info(f"client {request.host} starts {obj['method']}")
-                getattr(self, obj["method"])(*obj["args"])
+                await getattr(self, obj["method"])(*obj["args"])
             elif msg.type == aiohttp.WSMsgType.ERROR:
                 self.logger.warning(f"Websocket connection closed with exception {ws.exception()}")
         request.app['websockets'].remove(ws)
@@ -101,14 +105,19 @@ class Backend():
     def schedule_send(self, text, endpoint="log"):
         """ Schedules a send on the websocket.
         """
-        asyncio.create_task(self.send(text, endpoint))
+        asyncio.run_coroutine_threadsafe(self.send(text, endpoint), self.loop)
             
-    def recipe_update(self):
+    def schedule_recipe_update(self):
         self.schedule_send(self.kernel.recipe.get_table(), "table")
 
-    def recipe_set_filename(self, etaobj, id, key):
+    async def recipe_update(self):
+        await self.send(self.kernel.recipe.get_table(), "table")
+
+    async def recipe_set_filename(self, etaobj, id, key):
         try:
-            self.kernel.load_eta(etaobj, compile=False)
+            loop = asyncio.get_running_loop()
+            await loop.run_in_executor(None, lambda: self.kernel.load_eta(etaobj, compile=False))
+
             import tkinter as tk
             from tkinter.filedialog import askopenfilename
             root = tk.Tk()
@@ -130,9 +139,97 @@ class Backend():
             root.destroy()
             if path != "":
                 self.kernel.recipe.set_parameter(key, path)
-            self.recipe_update()
+            await self.recipe_update()
 
         except Exception as e:
+            self.logger.error(str(e), exc_info=True)
+
+    async def compile_eta(self, etaobj=None):
+        await self.send("none", "discard")  # show a neutral icon
+
+        loop = asyncio.get_running_loop()
+        await loop.run_in_executor(None, lambda: self.kernel.load_eta(etaobj))
+
+    async def process_eta(self, etaobj=None, id="code", group="main"):
+        await self.send("none", "discard")  # show a neutral icon
+
+        if self.displaying:
+            self.logfrontend.info(
+                "ETA.display: Script Panel is serving at http://{}:{}.".format(self.hostip, self.hostdashport))
+            self.logfrontend.warning(
+                "The current script is not executed, because a previously executed script is still serving the results.")
+            await self.send("http://{}:{}".format(self.hostip,
+                                                  self.hostdashport), "dash")
+        else:
+            loop = asyncio.get_running_loop()
+            try:
+                await loop.run_in_executor(None, lambda: self.kernel.load_eta(etaobj))
+            except ETACompilationException:
+                pass
+
+            if self.kernel.compilecache_nfunc is not None:
+                # ETA File version check
+                if self.kernel.recipe.get_parameter("ETA_VERSION") is not None and self.kernel.recipe.get_parameter("ETA_VERSION") != self.ETA_VERSION:
+                    self.logfrontend.warning(
+                        "ETA_VERSION: the recipe requires {} while ETA Backend is {}, you might encounter compatibility issues.".format(self.kernel.recipe.get_parameter("ETA_VERSION"), self.ETA_VERSION))
+
+                self.logfrontend.info(
+                    "Executing code in Script Panel in group {}...".format(group))
+                try:
+                    glob = {"eta": self.kernel}
+                    # side configuration panel
+                    if group in self.kernel.compilecache_vars:
+                        loc = self.kernel.compilecache_vars[group]
+                    else:
+                        loc = {}
+                    await loop.run_in_executor(None, lambda: exec(etaobj[id], glob, loc))
+                    await self.recipe_update()
+                except Exception as e:
+                    if (str(type(e)).find("numba") >= 0):
+                        self.logger.error(str(e), exc_info=True)
+                        self.logfrontend.error(
+                            "An internal error has occurred when numba is compiling the embedded code.")
+                    else:
+                        self.logfrontend.error(
+                            "This error is caused by user-written code in the Script Panel, instead of ETA itself.", exc_info=True)
+                        self.logger.error(str(e), exc_info=True)
+                    return
+                self.logfrontend.info(
+                    "Don't forget to save the recipe and share it!")
+
+    def show(self, app=None):
+        """ Displays a bokeh object or application. 
+        """
+
+        self.logfrontend.info("eta.show: Show bokeh object")
+        try:
+            global bokserver
+
+            def shutdown(doc):
+                bokserver.unlisten()
+                bokserver.stop()
+                self.displaying = False
+                self.logfrontend.info("Dashboard shutting down.")
+                self.schedule_send("http://{}:{}".format(self.hostip,
+                                                        self.hostdashport), "discard")
+            bokserver = Server(
+                        {"/": app, 
+                            "/shutdown": shutdown},
+                        address=self.hostip,
+                        port=self.hostdashport,
+                        allow_websocket_origin=[f"{self.hostip}:{self.hostport}", 
+                                                f"{self.hostip}:{self.hostdashport}"]
+                        )
+            bokserver.start()
+            
+            self.logfrontend.info(
+                "ETA.display: Script Panel is serving at http://{}:{}.".format(self.hostip, self.hostdashport))
+            self.schedule_send("http://{}:{}".format(self.hostip,
+                                                        self.hostdashport), "dash")
+            self.displaying = True
+        except Exception as e:
+            self.logfrontend.error("bokeh failed to start", exc_info=True)
+            self.displaying = False
             self.logger.error(str(e), exc_info=True)
 
     def display(self, app=None):
@@ -156,13 +253,13 @@ class Backend():
                         self.displaying = False
                         self.logfrontend.info("Dashboard shutting down.")
                         self.schedule_send("http://{}:{}".format(self.hostip,
-                                                                 self.hosthttpport), "discard")
+                                                        self.hostdashport), "discard")
                         response = app.server.make_response('Hello, World')
                         response.headers.add(
                             'Access-Control-Allow-Origin', '*')
                         return response
                     thread2 = threading.Thread(
-                        target=app.server.run, kwargs={'host': self.hostlisten, "port": int(self.hosthttpport)})
+                        target=app.server.run, kwargs={'host': self.hostip, "port": int(self.hostdashport)})
                     thread2.daemon = True
                     thread2.start()
                 elif str(type(app)) == "<class 'function'>":
@@ -173,82 +270,26 @@ class Backend():
                     def shutdown(doc):
                         bokserver.unlisten()
                         bokserver.stop()
-                        bokserver.io_loop.stop()
                         self.displaying = False
                         self.logfrontend.info("Dashboard shutting down.")
                         self.schedule_send("http://{}:{}".format(self.hostip,
-                                                                 self.hosthttpport), "discard")
-
-                    import asyncio
-                    asyncio.set_event_loop(asyncio.new_event_loop())
+                                                        self.hostdashport), "discard")
 
                     bokserver = Server(
-                        {"/": app, "/shutdown": shutdown}, address=self.hostlisten, port=int(self.hosthttpport))
+                        {"/": app, "/shutdown": shutdown}, address=self.hostip, port=int(self.hostdashport))
                     bokserver.start()
-                    thread3 = threading.Thread(target=bokserver.io_loop.start)
-                    thread3.daemon = True
-                    thread3.start()
                 else:
                     self.logfrontend.warning(
                         "eta.display: unknown type {} for display".format(str(type(app))))
                 self.logfrontend.info(
-                    "ETA.display: Script Panel is serving at http://{}:{}.".format(self.hostip, self.hosthttpport))
+                    "ETA.display: Script Panel is serving at http://{}:{}.".format(self.hostip, self.hostdashport))
                 self.schedule_send("http://{}:{}".format(self.hostip,
-                                                         self.hosthttpport), "dash")
+                                                self.hostdashport), "dash")
                 self.displaying = True
             except Exception as e:
                 self.logfrontend.error("bokeh failed to start", exc_info=True)
                 self.displaying = False
                 self.logger.error(str(e), exc_info=True)
-
-    def compile_eta(self, etaobj=None):
-        self.schedule_send("none", "discard")  # show a neutral icon
-        self.kernel.load_eta(etaobj)
-
-    def process_eta(self, etaobj=None, id="code", group="main"):
-        self.schedule_send("none", "discard")  # show a neutral icon
-        if self.displaying:
-            self.logfrontend.info(
-                "ETA.display: Script Panel is serving at http://{}:{}.".format(self.hostip, self.hosthttpport))
-            self.logfrontend.warning(
-                "The current script is not executed, because a previously executed script is still serving the results.")
-            self.schedule_send("http://{}:{}".format(self.hostip,
-                                                     self.hosthttpport), "dash")
-        else:
-            try:
-                self.kernel.load_eta(etaobj)
-            except ETACompilationException:
-                pass
-
-            if self.kernel.compilecache_nfunc is not None:
-                # ETA File version check
-                if self.kernel.recipe.get_parameter("ETA_VERSION") is not None and self.kernel.recipe.get_parameter("ETA_VERSION") != self.ETA_VERSION:
-                    self.logfrontend.warning(
-                        "ETA_VERSION: the recipe requires {} while ETA Backend is {}, you might encounter compatibility issues.".format(self.kernel.recipe.get_parameter("ETA_VERSION"), self.ETA_VERSION))
-
-                self.logfrontend.info(
-                    "Executing code in Script Panel in group {}...".format(group))
-                try:
-                    glob = {"eta": self.kernel}
-                    # side configuration panel
-                    if group in self.kernel.compilecache_vars:
-                        loc = self.kernel.compilecache_vars[group]
-                    else:
-                        loc = {}
-                    exec(etaobj[id], glob, loc)
-                    self.recipe_update()
-                except Exception as e:
-                    if (str(type(e)).find("numba") >= 0):
-                        self.logger.error(str(e), exc_info=True)
-                        self.logfrontend.error(
-                            "An internal error has occurred when numba is compiling the embedded code.")
-                    else:
-                        self.logfrontend.error(
-                            "This error is caused by user-written code in the Script Panel, instead of ETA itself.", exc_info=True)
-                        self.logger.error(str(e), exc_info=True)
-                    return
-                self.logfrontend.info(
-                    "Don't forget to save the recipe and share it!")
 
 
 class FrontendFormatter(logging.Formatter):
