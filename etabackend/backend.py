987443fdc97ec102124e65477c23128faf1570ce
Zuzeng Lin <linzuzeng@tju.edu.cn>
2020-03-14 17:28:46 +0800

    gui: fix plotly display and discard

--- etabackend/backend.py
+++ etabackend/backend.py
@@ -31,6 +31,7 @@ ETA_BANNER = \
 
 BASE_DIR = pathlib.Path(__file__).parent
 
+
 class Backend():
     def __init__(self, run_forever=True):
         self.ETA_VERSION = ETA_VERSION
@@ -42,63 +43,78 @@ class Backend():
         self.hostdashport = int(os.environ.get('ETA_DASHPORT') or "5679")
         self.not_displaying = threading.Event()
         self.not_displaying.set()
-
+        self.display_url = None
+        self.display_shutdown_url = None
 
         # Fix for tornado webserver
-        if hasattr(asyncio,"WindowsSelectorEventLoopPolicy"):
-            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+        if hasattr(asyncio, "WindowsSelectorEventLoopPolicy"):
+            asyncio.set_event_loop_policy(
+                asyncio.WindowsSelectorEventLoopPolicy())
 
-        self.app = web.Application(logger=logging.getLogger("etabackend.aiohttp"))
+        self.app = web.Application(
+            logger=logging.getLogger("etabackend.aiohttp"))
         self.app['websockets'] = weakref.WeakSet()
         self.app.add_routes([web.get('/', self.web_redirect),
-                             web.get('/index.html', self.web_index, name='default'),
+                             web.get('/index.html', self.web_index,
+                                     name='default'),
                              web.get('/ws', self.websocket_handler),
-                             web.get('/shutdown-display', self.shutdown_display),
+                             web.get('/shutdown-display',
+                                     self.shutdown_display),
                              web.get('/shutdown', self.shutdown),
-                             web.static('/', BASE_DIR / 'static/', append_version=True),
-                            ])
+                             web.static('/', BASE_DIR / 'static/',
+                                        append_version=True),
+                             ])
         self.logfrontend.addHandler(WebClientHandler(self.schedule_send_text))
 
         self.kernel = ETA()
-        self.kernel.add_callback('running', lambda: self.schedule_send({'op': 'running'}))
-        self.kernel.add_callback('paused', lambda: self.schedule_send({'op': 'paused'}))
-        self.kernel.add_callback('finished', lambda: self.schedule_send({'op': 'finished'}))
-        self.kernel.add_callback('update-recipe', lambda: self.schedule_recipe_update())
+        self.kernel.add_callback(
+            'running', lambda: self.schedule_send({'op': 'running'}))
+        self.kernel.add_callback(
+            'paused', lambda: self.schedule_send({'op': 'paused'}))
+        self.kernel.add_callback(
+            'finished', lambda: self.schedule_send({'op': 'finished'}))
+        self.kernel.add_callback(
+            'update-recipe', lambda: self.schedule_recipe_update())
 
         # Monkeypatch a display function to the kernel to support display in recipe.
         # FIXME This is somehow evil, but works. Should be fixed if the Display function is every fixed, it is also not so beautiful.
         self.kernel.display = self.display
         self.kernel.send = self.schedule_send_text
 
-        self.logger.info("ETA URL: http://{}:{}".format(self.hostip, self.hostport))
+        self.logger.info(
+            "ETA URL: http://{}:{}".format(self.hostip, self.hostport))
         self.loop = asyncio.get_event_loop()
 
         self.app.on_shutdown.append(self.on_shutdown)
 
         if run_forever:
-            web.run_app(self.app, host=self.hostip, port=self.hostport, shutdown_timeout=1, access_log=None)
+            web.run_app(self.app, host=self.hostip, port=self.hostport,
+                        shutdown_timeout=1, access_log=None)
 
     async def web_redirect(self, request):
         location = request.app.router['default'].url_for()
         raise web.HTTPFound(location=location)
-    
+
     async def web_index(self, request):
         return web.FileResponse(BASE_DIR / 'static' / 'index.html')
-    
+
     async def websocket_handler(self, request):
         ws = web.WebSocketResponse(autoping=False)
         await ws.prepare(request)
         request.app['websockets'].add(ws)
 
-        self.logger.info(f"New websocket client {request.host} connected to port {self.hostport}.")
+        self.logger.info(
+            f"New websocket client {request.host} connected to port {self.hostport}.")
         try:
             async for msg in ws:
                 if msg.type == aiohttp.WSMsgType.TEXT:
                     obj = msg.json()
-                    self.logger.info(f"client {request.host} starts {obj['method']}")
+                    self.logger.info(
+                        f"client {request.host} starts {obj['method']}")
                     await getattr(self, obj["method"])(*obj["args"])
                 elif msg.type == aiohttp.WSMsgType.ERROR:
-                    self.logger.warning(f"Websocket connection closed with exception {ws.exception()}")
+                    self.logger.warning(
+                        f"Websocket connection closed with exception {ws.exception()}")
         finally:
             request.app['websockets'].discard(ws)
             self.logger.info(f"Client {request.host} disconnected.")
@@ -115,7 +131,7 @@ class Backend():
         """
         for _ws in self.app['websockets']:
             await _ws.send_json(data)
-    
+
     def schedule_send(self, data):
         """ Schedules a send on the websocket.
         """
@@ -127,7 +143,8 @@ class Backend():
         self.schedule_send({'op': endpoint, 'msg': text})
 
     def schedule_recipe_update(self):
-        self.schedule_send({'op': 'table', 'table': self.kernel.recipe.get_table()})
+        self.schedule_send(
+            {'op': 'table', 'table': self.kernel.recipe.get_table()})
 
     async def recipe_update(self):
         await self.send({'op': 'table', 'table': self.kernel.recipe.get_table()})
@@ -177,13 +194,13 @@ class Backend():
                 "ETA.display: Script Panel is serving at http://{}:{}.".format(self.hostip, self.hostdashport))
             self.logfrontend.warning(
                 "The current script is not executed, because a previously executed script is still serving the results.")
-            await self.send({"op": "dash", 
-                             "url-dash": "http://{}:{}".format(self.hostip, self.hostdashport),
-                             "url-shutdown": "http://{}:{}/shutdown-display".format(self.hostip, self.hostport)})
+            await self.send({"op": "dash",
+                             "url-dash": self.display_url,
+                             "url-shutdown": self.display_shutdown_url})
         else:
             loop = asyncio.get_running_loop()
             await loop.run_in_executor(None, lambda: self.kernel.load_eta(etaobj))
-            
+
             if self.kernel.compilecache_nfunc is not None:
                 # ETA File version check
                 if self.kernel.recipe.get_parameter("ETA_VERSION") is not None and self.kernel.recipe.get_parameter("ETA_VERSION") != self.ETA_VERSION:
@@ -221,27 +238,32 @@ class Backend():
         """
         import tornado.ioloop
         from bokeh.server.server import Server
-        
+
         self.logfrontend.info("show_bokeh: Show bokeh object in main thread")
         try:
             self.app['bokserver'] = Server(
-                        {"/": app,},
-                        address=self.hostip,
-                        port=self.hostdashport,
-                        allow_websocket_origin=[f"{self.hostip}:{self.hostport}", 
-                                                f"{self.hostip}:{self.hostdashport}",
-                                                f"127.0.0.1:{self.hostport}",
-                                                f"127.0.0.1:{self.hostdashport}"
-                                               ],
-                        io_loop=tornado.ioloop.IOLoop.current()
-                        )
+                {"/": app, },
+                address=self.hostip,
+                port=self.hostdashport,
+                allow_websocket_origin=[f"{self.hostip}:{self.hostport}",
+                                        f"{self.hostip}:{self.hostdashport}",
+                                        f"127.0.0.1:{self.hostport}",
+                                        f"127.0.0.1:{self.hostdashport}"
+                                        ],
+                io_loop=tornado.ioloop.IOLoop.current()
+            )
             self.app['bokserver'].start()
             logging.getLogger('tornado.access').setLevel(logging.WARNING)
 
-            self.logfrontend.info("ETA.display: Script Panel is serving at http://{}:{}.".format(self.hostip, self.hostdashport))
-            await self.send({"op": "dash", 
-                             "url-dash": "http://{}:{}".format(self.hostip, self.hostdashport),
-                             "url-shutdown": "http://{}:{}/shutdown-display".format(self.hostip, self.hostport)})
+            self.display_shutdown_url = "http://{}:{}/shutdown-display".format(
+                self.hostip, self.hostport)
+            self.display_url = "http://{}:{}".format(
+                self.hostip, self.hostdashport)
+            self.logfrontend.info(
+                "ETA.display: Script Panel is serving at {}.".format(self.display_url))
+            await self.send({"op": "dash",
+                             "url-dash": self.display_url,
+                             "url-shutdown": self.display_shutdown_url})
 
         except Exception as e:
             self.logfrontend.error("bokeh failed to start", exc_info=True)
@@ -254,14 +276,13 @@ class Backend():
             self.app['bokserver'] = None
             self.not_displaying.set()
             self.logfrontend.info("Dashboard shutting down.")
-            self.schedule_send_text("http://{}:{}".format(self.hostip,
-                                                          self.hostport), "discard")
+            self.schedule_send_text("", "discard")
             return web.json_response(['success'])
-        
+
         return web.json_response([])
 
     async def shutdown(self, request):
-        os.kill(os.getpid(), signal.SIGINT) #FIXME if there is a better way.
+        os.kill(os.getpid(), signal.SIGINT)  # FIXME if there is a better way.
         return web.Response(text="ETA backend is shutdown.")
 
     def display(self, app=None, type='dash'):
@@ -284,7 +305,7 @@ class Backend():
                         func()
                         self.not_displaying.set()
                         self.logfrontend.info("Dashboard shutting down.")
-                        self.schedule_send_text(f"http://{self.hostip}:{self.hostport}", "discard")
+                        self.schedule_send_text("", "discard")
                         response = app.server.make_response('Hello, World')
                         response.headers.add(
                             'Access-Control-Allow-Origin', '*')
@@ -292,19 +313,22 @@ class Backend():
                     thread2 = threading.Thread(
                         target=app.server.run, kwargs={'host': self.hostip, "port": int(self.hostdashport)})
                     thread2.daemon = True
-                    thread2.start()                   
-                    self.logfrontend.info(f"ETA.display: Script Panel is serving at http://{self.hostip}:{self.hostdashport}.")
-                    self.schedule_send({"op": "dash", 
-                                        "url-dash": f"http://{self.hostip}:{self.hostdashport}",
-                                        "url-shutdown": f"http://{self.hostip}:{self.hostport}/shutdown-display"})
+                    thread2.start()
+
+                    self.display_url = f"http://{self.hostip}:{self.hostdashport}"
+                    self.display_shutdown_url = f"http://{self.hostip}:{self.hostdashport}/shutdown"
+                    self.logfrontend.info(
+                        f"ETA.display: Script Panel is serving at {self.display_url}.")
+                    self.schedule_send({"op": "dash",
+                                        "url-dash": self.display_url,
+                                        "url-shutdown": self.display_shutdown_url})
                 elif type == "bokeh":
-                    asyncio.run_coroutine_threadsafe(self.show_bokeh(app), self.loop)
+                    asyncio.run_coroutine_threadsafe(
+                        self.show_bokeh(app), self.loop)
                 else:
                     self.logfrontend.warning(
                         "eta.display: unknown type {} for display".format(str(type(app))))
                     return None
-
-
 
                 self.not_displaying.clear()
                 return self.not_displaying
