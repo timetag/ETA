5d08c3de8a9be3cc0fdb134a19f7094447517e68
Zuzeng Lin <linzuzeng@tju.edu.cn>
2020-04-01 21:30:00 +0800

    packaging: bump to 0.7.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="etabackend",
-    version="0.7.1",
+    version="0.7.2",
     author="ETA Team",
     description="Extensible Timetag Analyzer",
     include_package_data=True,
