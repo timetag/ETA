dfc84a69488e6e58b09d5ce1536bfb81bbc1fa33
Lucas Schweickert <28017534+Cyberfly100@users.noreply.github.com>
2020-06-15 15:00:54 +0200

    initial commit
    utilities for use in eta code panels

---
+++ etabackend/tk/utils.py
@@ -0,0 +1,40 @@
+""" Utilities for use in ETA code panels.
+"""
+import pathlib
+
+def info(globals_dict, recipe_type=None):
+    '''
+    generates an informative header for exported data files with content specific to the recipe.
+    
+    Parameters
+    ----------
+    recipe_type : STR, optional
+        selector for recipe type. The default is None.
+    timeres_path : PATH or STRING, optional
+        path of file used for creating the histogram. The default is f.
+    binsize : INT, optional
+        binsize in ps. The default is binsize.
+    bins : INT, optional
+        number of bins in histogram. The default is bins.
+
+    Returns
+    -------
+    string : STR
+        formatted header information.
+
+    !the in globals() thing should be removed once we separate recipes depending on hardware!
+    '''
+    timeres_path = globals_dict.get("f", "not availible")
+    binsize = globals_dict.get("binsize", "not availible")
+    bins = globals_dict.get("bins", "not availible")
+    types = {'correlation':f'mode: {globals_dict.get("HH400_mode","quTAG")}\n',
+             'lifetime':f'mode: {globals_dict.get("ch_sel","not availible")}\n',
+             'simulation':f'syncs: {globals_dict.get("syncs","not availible")}\n',
+             'countrate':''}
+    string = (f'Created with ETA - https://timetag.github.io\n'
+        f'filename: {timeres_path.name if isinstance(timeres_path, pathlib.PurePath) else timeres_path}\n'
+        f'binsize: {binsize}\n'
+        f'bins: {bins}\n')
+    if recipe_type:
+        string += types[recipe_type]
+    return string
