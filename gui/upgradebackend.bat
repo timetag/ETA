ef6b851a461a1d36ab88cca17ad5b8521011a26b
Zuzeng Lin <linzuzeng@users.noreply.github.com>
2021-12-22 14:36:54 +0800

    gui: pip uninstall before install

--- gui/upgradebackend.bat
+++ gui/upgradebackend.bat
@@ -1,3 +1,4 @@
 python -m ensurepip 
-python -m pip --disable-pip-version-check install --find-links=./etabackend  etabackend --upgrade --force-reinstall
+python -m pip uninstall etabackend -y
+python -m pip --disable-pip-version-check install --find-links=./etabackend  etabackend --upgrade
 pause
