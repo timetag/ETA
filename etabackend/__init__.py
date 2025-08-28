dbf5b429ebe2ecdb633e0bb27f373a4aee87fb2f
Samuel Gyger <samuel@gyger.tech>
2025-08-27 19:29:32 -0700

    backend: Change way of discovering package version at runtime
    At least Python 3.8 now.

--- etabackend/__init__.py
+++ etabackend/__init__.py
@@ -1,15 +1,11 @@
 import logging
 from logging import NullHandler
-from pkg_resources import get_distribution, DistributionNotFound
+from importlib.metadata import version, PackageNotFoundError
 
 try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
+    __version__ = version(__name__)
+except PackageNotFoundError:
     __version__ = 'unknown'
-finally:
-    del get_distribution, DistributionNotFound
 
 # Set default logging handler to avoid "No handler found" warnings.
 logging.getLogger(__name__).addHandler(NullHandler())
